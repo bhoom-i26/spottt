c99b6f2768e9bf9e6f3de60c5908ee8e0a0b166d
bhoom-i26 <bhoomisharma369@gmail.com>
2026-03-24 00:00:14 +0530

    base layout

---
+++ components/header.jsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <nav className='fixed top-0 left-0 right-0 bg-background/80 backdrrop-blur-xl z-20 border-b'>
+       <div className='max-w-7xl mx-auto px-6 py-4 flex items-center justify-between'>
+        {/* logo */}
+        <Link href={"/"} className='flex items-center'>
+        <Image
+         src="/spott.png" 
+         alt='spott logo' 
+         width={500} 
+         height={500} 
+         className='w-full h-11'
+         priority
+         />
+         {/* pro badge */}
+         </Link>
+        {/* search & location - Desktop only */}
+        {/* right side actions */}
+       </div>
+        {/* mobile search & location -below header */}
+    </nav>
+        {/* modals */}
+    </>
+    
+  )
+}
+
+export default Header
